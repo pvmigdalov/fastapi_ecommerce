faccd071f9c97353fed087603d71915a86eb9626
Petr <pvmigdalov@yandex.ru>
2025-01-25 22:33:26 +0300

    Create carcass CRUD for products

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
 from app.routers import category_router
+from app.routers import products_router
 
 app = FastAPI()
+
+@app.get("/")
+async def welcome():
+    return {"message": "My e-commerce app"}
+
 app.include_router(category_router)
+app.include_router(products_router)
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,1 +1,2 @@
 from app.routers.category import router as category_router
+from app.routers.products import router as products_router

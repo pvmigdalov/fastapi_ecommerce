fc77964c26038e23b2d578371d5e5d531b0dd71f
Petr <pvmigdalov@yandex.ru>
2025-01-26 17:24:28 +0300

    Move common attributes to Base

--- app/database/db.py
+++ app/database/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, create_engine
+from sqlalchemy import Boolean, Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 
@@ -7,4 +7,6 @@ session = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     id = Column(Integer, primary_key=True, index=True)
-
+    name = Column(String)
+    slug = Column(String, unique=True, index=True)
+    is_active = Column(Boolean, default=True)
--- app/models.py
+++ app/models.py
@@ -7,19 +7,12 @@ from app.database import Base
 class Category(Base):
     __tablename__ = "categories"
 
-    name = Column(String)
-    slug = Column(String, unique=True, index=True)
-    is_active = Column(Boolean, default=True)
 
 class Product(Base):
     __tablename__ = 'products'
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    slug = Column(String, unique=True, index=True)
     description = Column(String)
     price = Column(Integer)
     image_url = Column(String)
     stock = Column(Integer)
     rating = Column(Float)
-    is_active = Column(Boolean, default=True)

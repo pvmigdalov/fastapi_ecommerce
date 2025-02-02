72fc543bfad7d2aef44a24174d60edbbe26e3779
Petr <pvmigdalov@yandex.ru>
2025-02-02 20:45:23 +0300

    Add if __name__ ... for debag in main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from app.routers import category_router
 from app.routers import products_router
@@ -11,3 +12,6 @@ async def welcome():
 
 app.include_router(category_router)
 app.include_router(products_router)
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", reload=True)

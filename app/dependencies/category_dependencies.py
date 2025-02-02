7275124a1a6dd1c7ca0486a6039b23dd28abc12f
Petr <pvmigdalov@yandex.ru>
2025-02-02 16:43:59 +0300

    Fixed bug in category_dependencies.py

--- app/dependencies/category_dependencies.py
+++ app/dependencies/category_dependencies.py
@@ -11,6 +11,6 @@ async def check_category_exists(
         session: Annotated[Session, Depends(get_db_session)], 
         category_id: int
 ):
-    category = await CategoryCrudManager.select_category_by_id(session, category_id)
+    category = await CategoryCrudManager.select_by_id(session, category_id)
     if category is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, "There is no category found")
